6e87f50c6ab66c91e9439b0b60ad7af9ae2a831d
Joabe Costa <joabetc@hotmail.com>
2022-11-02 22:32:25 +0100

    Adjust class

--- src/main/java/com/github/joergpfruender/junitgoodies/ParameterizedTestHelper.java
+++ src/main/java/com/github/joergpfruender/junitgoodies/ParameterizedTestHelper.java
@@ -1,5 +1,6 @@
 package com.github.joergpfruender.junitgoodies;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.ArrayList;
@@ -7,22 +8,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-/*
-   Copyright 2019 Jörg Pfründer
+/**
+ Copyright 2019 Jörg Pfründer
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+ <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-
- */
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ **/
 public class ParameterizedTestHelper {
 
   private ParameterizedTestHelper() { }
@@ -36,19 +36,17 @@ public class ParameterizedTestHelper {
    * @param b stream of JUnit5 arguments
    * @return the cartesian product where each tuple of arguments of stream a is multiplied by the tuple of arguments of stream b
    */
-  @SuppressWarnings("unchecked")
-  public static Stream<Arguments> cartesian(Stream a, Stream b) {
-    List argumentsA = (List) a.collect(Collectors.toList());
-    List argumentsB = (List) b.collect(Collectors.toList());
+  public static <T, O> Stream<Arguments> cartesian(Stream<T> a, Stream<O> b) {
+    List<T> argumentsA = a.collect(Collectors.toList());
+    List<O> argumentsB = b.collect(Collectors.toList());
 
     List<Arguments> result = cartesian(argumentsA, argumentsB);
 
     return result.stream();
   }
 
-  @SuppressWarnings("unchecked")
-  static List<Arguments> cartesian(List argumentsA, List argumentsB) {
-    List<Arguments> result = new ArrayList();
+  static <T, O> List<Arguments> cartesian(List<T> argumentsA, List<O> argumentsB) {
+    List<Arguments> result = new ArrayList<>();
     for (Object o : argumentsA) {
       Object[] objects = asArray(o);
       for (Object o1 : argumentsB) {
@@ -70,5 +68,4 @@ public class ParameterizedTestHelper {
     }
     return objects;
   }
-
 }
