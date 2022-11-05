61303d2350f866dbec93861d1eb16cc588a61069
Joabe Costa <joabetc@hotmail.com>
2022-11-05 17:18:33 +0100

    Revert change

--- src/test/java/com/github/joergpfruender/junitgoodies/ParameterizedTestHelperTest.java
+++ src/test/java/com/github/joergpfruender/junitgoodies/ParameterizedTestHelperTest.java
@@ -8,14 +8,14 @@ import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/**
+/*
    Copyright 2019 Jörg Pfründer
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
 
-       <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a>
+       http://www.apache.org/licenses/LICENSE-2.0
 
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
@@ -23,7 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
    See the License for the specific language governing permissions and
    limitations under the License.
 
- **/
+ */
 class ParameterizedTestHelperTest {
 
   public static Stream<Arguments> additionSamples() {
